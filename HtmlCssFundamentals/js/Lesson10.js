b13d849f345f1cbb45d52a9ba919d28e30922348
Daniel Mackay <crankage@gmail.com>
2014-01-26 10:41:30 +1100

    initial

---
+++ HtmlCssFundamentals/js/Lesson10.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../Scripts/jquery-1.8.2.js" />
+
+$(function () {
+    $('#clearToppings').click(function () {
+        var toppingInputs = $('.topping');
+        toppingInputs.attr('checked', false);
+    });
+});
